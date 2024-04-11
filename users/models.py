c9530b1a4bb0597669594ff2da11dcb974a17d4e
NarasimhaRaju Lakkasana <lakkasn@clarkson.edu>
2024-04-11 09:46:34 -0400

    Updated Users model

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    # User id - unique field mapping to every user
     user_id = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
